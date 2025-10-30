83be9b43d37c3187c93e960016e3c814f935568b
erkangs86 <98686790+erkangs86@users.noreply.github.com>
2025-10-31 01:28:59 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-30T22:28:53.035Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-10-30T22:28:59.431Z
+Commit: Fix memory leak in event handlers
