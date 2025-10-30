20c248252a8f4b69305a5bb03946a1e7133e9e66
erkangs86 <98686790+erkangs86@users.noreply.github.com>
2025-10-31 01:28:55 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-30T22:28:47.179Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-10-30T22:28:54.493Z
+Commit: Fix race condition in async operations
